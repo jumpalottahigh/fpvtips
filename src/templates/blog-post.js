0a47bd8db54b33b2d8a9a12a0035cf91526da471
Georgi Yanev <georgiyanev.gy@gmail.com>
2018-10-07 00:49:04 +0300

    Improve blog styles

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
 import { graphql } from 'gatsby'
 import Helmet from 'react-helmet'
 import Layout from '../components/Layout/layout'
+
+const BlogContent = styled.div`
+  text-align: left;
+  max-width: 80ch;
+  margin: 0 auto;
+`
 
 class BlogPostTemplate extends React.Component {
   render() {
@@ -22,7 +29,7 @@ class BlogPostTemplate extends React.Component {
         </Helmet>
         <h1>{post.frontmatter.title}</h1>
         <p>{post.frontmatter.date}</p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <BlogContent dangerouslySetInnerHTML={{ __html: post.html }} />
         <hr />
       </Layout>
     )
