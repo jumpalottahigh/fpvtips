e609195b7bb5ed53638d81ebfb68522a90941f4c
Georgi Yanev <georgiyanev.gy@gmail.com>
2018-10-21 18:49:43 +0300

    Add video options to sections

--- src/components/UI/Section.js
+++ src/components/UI/Section.js
@@ -4,10 +4,12 @@ import { Link } from 'gatsby'
 import Button from '@material-ui/core/Button'
 
 const StyledSection = styled.section`
-  padding: 2rem 0;
+  padding: 3.5rem 0;
   color: ${props => (props.color ? props.color + '!important' : '')};
   background-color: ${props =>
     props.bgcolor ? props.bgcolor + '!important' : ''};
+  background-image: ${props =>
+    props.bgimage ? 'url(' + props.bgimage + ')!important' : ''};
 
   h3 {
     margin-top: 0;
@@ -29,12 +31,32 @@ export default class Section extends React.Component {
   }
 
   render() {
-    const { color, bgcolor, title, content, buttons } = this.props.data
+    const {
+      color,
+      bgcolor,
+      bgimage,
+      video,
+      title,
+      content,
+      buttons,
+    } = this.props.data
 
     return (
-      <StyledSection color={color} bgcolor={bgcolor}>
+      <StyledSection color={color} bgcolor={bgcolor} bgimage={bgimage}>
         <h3>{title}</h3>
         <p>{content}</p>
+        {video && (
+          <div style={{ maxWidth: '640px', margin: '0 auto' }}>
+            <iframe
+              width="100%"
+              height="315"
+              src={video}
+              frameBorder="0"
+              title="Flying FPV video"
+              allowFullScreen=""
+            />
+          </div>
+        )}
         {buttons && (
           <div className="cta-container">
             {buttons.map((button, index) => {
