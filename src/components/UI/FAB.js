ee7e49750a4181b40ca37f403016ed0aff236321
Georgi Yanev <georgiyanev.gy@gmail.com>
2018-12-07 00:28:58 +0200

    Upgrade dependencies

--- src/components/UI/FAB.js
+++ src/components/UI/FAB.js
@@ -1,23 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
-import Button from '@material-ui/core/Button'
 import AddIcon from '@material-ui/icons/Add'
+import Fab from '@material-ui/core/Fab'
 
-const StyledButton = styled(Button)`
+const StyledButton = styled(Fab)`
   position: fixed !important;
   right: 2rem !important;
   bottom: 5rem !important;
 `
 
-const FAB = props => (
-  <StyledButton
-    onClick={props.onClick}
-    variant="fab"
-    color="primary"
-    aria-label="Add"
-  >
+const StyledFab = props => (
+  <StyledButton onClick={props.onClick} color="primary" aria-label="Add">
     <AddIcon />
   </StyledButton>
 )
 
-export default FAB
+export default StyledFab
